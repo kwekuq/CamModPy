0ddca1190d79ea63753b2a510566edc203301c8e
Kweku Quansah <kwekuq@gmail.com>
2014-03-08 21:23:29 +0200

    Added converter class to project. No conversion code added, just the holding class.

---
+++ CamModPy/Converter.py
@@ -0,0 +1,22 @@
+'''
+Created on 08 Mar 2014
+
+@author: kwekuq
+'''
+class video():
+    command = "";
+
+    def __init__(self):
+        self.command = "";
+        
+    def transpose(self, video):
+        self.command = self.command + video;
+        
+class picture():
+    command = "";
+
+    def __init__(self):
+        self.command = "";
+        
+    def transpose(self, video):
+        self.command = self.command + video;
