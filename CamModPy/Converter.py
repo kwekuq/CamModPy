b36e6bb608b30c85ad72245603574236d169a7b6
Kweku Quansah <kwekuq@gmail.com>
2014-03-15 19:57:52 +0200

    Changes to convert and delete of old test modules

--- CamModPy/Converter.py
+++ CamModPy/Converter.py
@@ -5,10 +5,18 @@ Created on 08 Mar 2014
 '''
 class video():
     command = "";
+    status = False;
 
     def __init__(self):
         self.command = "MP$Box -add ";
         
     def transpose(self, video):
-        self.command = self.command + video + ".h264 " + video + ".mp4";
+        if(self.status == False):
+            self.command = self.command + video + ".h264 " + video + ".mp4";
+            self.status = True;
+        else:
+            print("Command already excuted. Please try running clean() to reset.");
         
+    def clean(self):
+        self.command = "MP$Box -add ";
+        self.status = False;
